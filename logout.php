98872697ea590b414b2000047e81198a393cc40d
Afolabi Temidayo Timothy <46169907+Afolabi8120@users.noreply.github.com>
2021-07-17 14:04:20 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,10 @@
 <?php
+        //Name: Afolabi Temidayo Timothy
+    //Intern ID: SH-IT-48472
+    //Stack: Web Development(Backend)
+    //Program: Side Hustle Internship 3.0 
+
+
 	include_once('./includes/session.php');
 	include_once('./includes/function.php');
 
